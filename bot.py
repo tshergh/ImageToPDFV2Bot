53dbb43725c3f99493c03ab5f559c2466e837b4e
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2021-09-05 13:00:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,11 +31,11 @@ async def start(client, message):
 
 i can convert image to pdf
 
-This Bot Created by @AswanthVK""",reply_to_message_id = message.message_id ,  #reply_markup=InlineKeyboardMarkup(
+This Bot Created by @AswanthVK""",reply_to_message_id = message.message_id ,  reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("Support Group" ,url="https://t.me/NewBotzSupport"),
-                 InlineKeyboardButton("Update Channel", url="https://t.me/NewBotz") ]       ]        ) )
+                    InlineKeyboardButton("Update Channel", url="https://t.me/NewBotz") ]       ]        ) )
 
 
 
