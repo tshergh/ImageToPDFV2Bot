fa8819ff61cc4a6efc901da0f725dc218d2dd65c
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-07-14 18:17:33 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,9 @@
+
+class Translation(object):
+
+    PROCESS_TXT = "<code>Processing your link..</code>"
+    INVALID_LINK_TXT = "<b>Invalid link</b>\n\n<i>Please send me a valid link</i>"
+    UPLOAD_TXT = "<b>Uploading your file..</b>"
+    ERROR_TXT = "<b>URL Error</b>\n\n<i>Unable to create a Pdf with this URL.\nTry again with a valid one..</i>"
+    CAPTION_TXT = "{}\n\n© @NewBotz"
+    THUMB_URL = "https://telegra.ph/file/f983772ef263293351b55.jpg"
