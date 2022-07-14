d3e89343b11409d10cab4aa398eb2d5edb444d53
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-07-14 18:47:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,14 +44,14 @@ else:
 @app.on_message(filters.command(['start']))
 async def start(client, message):
  #await client.send_message(LOG_CHANNEL, f"**New User Joined:** \n\nUser [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started Bot!!")
- await message.reply_text(text=f"""{wish}
+ await message.reply_text(text=f"""
 Hello [{message.from_user.first_name }](tg://user?id={message.from_user.id})
 
-i can convert image to pdf & Web URL to pdf."""),reply_to_message_id = message.message_id) # ,  reply_markup=InlineKeyboardMarkup(
-            #[
-                #[
-                    #InlineKeyboardButton("Support Group" ,url="https://t.me/NewBotzSupport"),
-                    #InlineKeyboardButton("Update Channel", url="https://t.me/NewBotz") ]       ]        ) )
+i can convert image to pdf & Web URL to pdf."""),reply_to_message_id = message.message_id) ,  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Support Group" ,url="https://t.me/NewBotzSupport"),
+                    InlineKeyboardButton("Update Channel", url="https://t.me/NewBotz") ]       ]        ) )
 
 
 
