680adde0fcb8c52af8311971b70d91f3b9a2271b
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2022-07-16 15:56:42 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,9 @@
 
 class Translation(object):
 
-    PROCESS_TXT = "<code>Processing your link..</code>"
-    INVALID_LINK_TXT = "<b>Invalid link</b>\n\n<i>Please send me a valid link</i>"
-    UPLOAD_TXT = "<b>Uploading your file..</b>"
-    ERROR_TXT = "<b>URL Error</b>\n\n<i>Unable to create a Pdf with this URL.\nTry again with a valid one..</i>"
-    CAPTION_TXT = "{}\n\n© @NewBotz"
+    PROCESS_TXT = "<code>معالجة الرابط...</code>"
+    INVALID_LINK_TXT = "<b>الرابط غير صالح</b>\n\n<i>ارجوك ارسل لي رابط صالح</i>"
+    UPLOAD_TXT = "<b>تحميل الرابط...</b>"
+    ERROR_TXT = "<b>خطا في رابك</b>\n\n<i>غير قادر على إنشاء ملف Pdf بعنوان URL هذا. \nحاول مرة أخرى باستخدام واحد صالح ..</i>"
+    CAPTION_TXT = "{}\n\n© @ooonn2"
     THUMB_URL = "https://telegra.ph/file/f983772ef263293351b55.jpg"
